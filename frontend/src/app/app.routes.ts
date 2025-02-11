86b7e512dfa9dd023a1068a0f8ca4c096c51e985
BrenoC4rvalho <bc101004@gmail.com>
2025-02-11 13:55:59 -0300

    feat: create page notFound

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/notFound/notFound.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundComponent}
 ];
