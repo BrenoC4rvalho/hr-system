9712700df5f1d8cf46688f9dfa66ef689520e516
BrenoC4rvalho <bc101004@gmail.com>
2025-02-11 20:27:08 -0300

    feat: create internal error serve page.

---
+++ frontend/src/app/pages/Error/serverError/serverError.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: 'app-server-error',
+  imports: [],
+  templateUrl: './serverError.component.html',
+})
+export class ServerErrorComponent {
+
+}
