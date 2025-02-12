9d32eb1570d01cc8028e7417864a7e0b5e406b3b
BrenoC4rvalho <bc101004@gmail.com>
2025-02-12 14:18:32 -0300

    feat: genereate template HTML for list employees component.

--- frontend/src/app/components/list-employees/list-employees.component.ts
+++ frontend/src/app/components/list-employees/list-employees.component.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list-employees',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-employees.component.html',
 })
 export class ListEmployeesComponent {
-
+  employees = [
+    { id: 1, name: 'John Fransico', phone: '1234567890', role: 'Developer', department: 'IT', shift: 'Morning' },
+    { id: 1, name: 'John Neymar', phone: '1234567890', role: 'Developer', department: 'IT', shift: 'Afternoon' },
+    { id: 1, name: 'John Rubens', phone: '1234567890', role: 'Developer', department: 'IT', shift: 'Night' },
+    { id: 1, name: 'John Caetan', phone: '1234567890', role: 'Developer', department: 'IT', shift: 'Morning' }
+  ]
 }
