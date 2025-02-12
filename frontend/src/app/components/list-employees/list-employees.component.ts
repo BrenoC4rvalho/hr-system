ca39dbdf2e9b7182167209786158ed5d2e944f3f
BrenoC4rvalho <bc101004@gmail.com>
2025-02-12 15:45:37 -0300

    feat: generate styles for list-employees-component.

--- frontend/src/app/components/list-employees/list-employees.component.ts
+++ frontend/src/app/components/list-employees/list-employees.component.ts
@@ -1,16 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LucideAngularModule, Eye, UserRoundSearch } from 'lucide-angular';
 
 @Component({
   selector: 'app-list-employees',
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './list-employees.component.html',
 })
 export class ListEmployeesComponent {
+
+  readonly EyeIcon = Eye;
+  readonly UserRoundSearchIcon = UserRoundSearch;
+
   employees = [
     { id: 1, name: 'John Fransico', phone: '1234567890', role: 'Developer', department: 'IT', shift: 'Morning' },
     { id: 1, name: 'John Neymar', phone: '1234567890', role: 'Developer', department: 'IT', shift: 'Afternoon' },
     { id: 1, name: 'John Rubens', phone: '1234567890', role: 'Developer', department: 'IT', shift: 'Night' },
     { id: 1, name: 'John Caetan', phone: '1234567890', role: 'Developer', department: 'IT', shift: 'Morning' }
-  ]
+  ];
 }
