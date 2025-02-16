1e6a0e81bc02101a10356d3da6277e6aa1c7bdcf
BrenoC4rvalho <bc101004@gmail.com>
2025-02-16 01:46:44 -0300

    feat: add departments table migration.

---
+++ backend/src/main/resources/db/migration/V2__create_departments_table.sql
@@ -0,0 +1,8 @@
+-- V2: Create Departments Table
+
+CREATE TABLE departments (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
