5fb68b4b66391dbaea046af413f651446d151170
BrenoC4rvalho <bc101004@gmail.com>
2025-02-16 21:10:04 -0300

    feat: add annotations for validation and hibernate for position model.

--- backend/src/main/java/com/example/backend/model/Position.java
+++ backend/src/main/java/com/example/backend/model/Position.java
@@ -1,10 +1,50 @@
 package com.example.backend.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "positions")
 public class Position {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "The name field cannot be blank.")
+    @Length(min = 2, max = 100, message = "The name field between 2 and 100 characters.")
     private String name;
+
+    @Length(max = 255, message = "The description field cannot be greater than 255 characters.") 
     private String description;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "position")
+    @JsonIgnore
+    private List<Employee> employees;
 
     public Position() {
     }
@@ -42,6 +82,18 @@ public class Position {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
     
 }
