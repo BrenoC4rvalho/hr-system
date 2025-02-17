0e9e2fd67bd01c3da069bde3c31308055281f3b4
BrenoC4rvalho <bc101004@gmail.com>
2025-02-16 21:10:35 -0300

    feat: add annotations for validation and hibernate for Employee Model.

--- backend/src/main/java/com/example/backend/model/Employee.java
+++ backend/src/main/java/com/example/backend/model/Employee.java
@@ -1,53 +1,134 @@
 package com.example.backend.model;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 import com.example.backend.enums.EmployeeStatus;
 import com.example.backend.enums.Gender;
 import com.example.backend.enums.Shift;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "employees")
 public class Employee {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    
+    private Long id;
+
+    @Column(name = "first_name",nullable = false)
+    @NotBlank
+    @Length(min = 3, max = 100, message = "The first name field between 3 and 100 characters.")
     private String firstName;
+    
+    @Column(name = "last_name")
+    @Length(max = 255, message = "The last name field cannot be greater than 255 characters.")
     private String lastName;
+
+    @Email(message = "The email must be a valid format.")
+    @Length(max = 255, message = "The email field cannot be greater than 255 characters.")
     private String email;
+    
+    @Column(nullable = false)
+    @NotBlank(message = "The phone field cannot be blank.")
+    @Length(max = 30, message = "The phone field cannot be greater than 30 characters.")
     private String phone;
+
+    @NotNull(message = "The gender field cannot be null.")
+    @Enumerated(EnumType.STRING)
     private Gender gender;
-    private int departmentId;
-    private int positionId;
-    private Shift shift;
+
+    @NotNull(message = "The department field cannot be null.")
+    @ManyToOne()
+    @JoinColumn(name = "department_id", referencedColumnName = "id", nullable = false)
+    private Department department;
+    
+    @NotNull(message = "The position id field cannot be null.")
+    @ManyToOne()
+    @JoinColumn(name = "position_id", referencedColumnName = "id", nullable = false)
+    private Position position;
+
+    @NotNull(message = "The shift field cannot be null.")
+    @Enumerated(EnumType.STRING)
+    private Shift shift = Shift.NONE;
+
+    @NotNull(message = "The status field cannot be null.")
+    @Enumerated(EnumType.STRING)
     private EmployeeStatus status = EmployeeStatus.ACTIVE;
+
+    @Column(name = "birth_date", nullable = false)
+    @NotNull(message = "The birth date field cannot be null.")
     private LocalDate birthDate;
-    private LocalDate hireDate;
+
+    @Column(name = "hired_date")
+    private LocalDate hiredDate;
+
+    @Column(name = "termination_date")
     private LocalDate terminationDate;
     
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "manager")
+    @JsonIgnore
+    private List<Department> departments;
+
+    @OneToOne(mappedBy = "employee")
+    @JsonIgnore
+    private User user;
+
     public Employee() {
     }
 
-    public Employee(int id, String firstName, String lastName, String email, String phone, Gender gender,
-            int departmentId, int positionId, Shift shift, EmployeeStatus status, LocalDate birthDate,
-            LocalDate hireDate, LocalDate terminationDate) {
+    public Employee(Long id, String firstName, String lastName, String email, String phone, Gender gender,
+            Department department, Position position, Shift shift, EmployeeStatus status, LocalDate birthDate,
+            LocalDate hiredDate, LocalDate terminationDate) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phone = phone;
         this.gender = gender;
-        this.departmentId = departmentId;
-        this.positionId = positionId;
+        this.department = department;
+        this.position = position;
         this.shift = shift;
         this.status = status;
         this.birthDate = birthDate;
-        this.hireDate = hireDate;
+        this.hiredDate = hiredDate;
         this.terminationDate = terminationDate;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -91,20 +172,20 @@ public class Employee {
         this.gender = gender;
     }
 
-    public int getDepartmentId() {
-        return departmentId;
+    public Department getDepartment() {
+        return department;
     }
 
-    public void setDepartmentId(int departmentId) {
-        this.departmentId = departmentId;
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 
-    public int getPositionId() {
-        return positionId;
+    public Position getPosition() {
+        return position;
     }
 
-    public void setPositionId(int positionId) {
-        this.positionId = positionId;
+    public void setPosition(Position position) {
+        this.position = position;
     }
 
     public Shift getShift() {
@@ -131,12 +212,12 @@ public class Employee {
         this.birthDate = birthDate;
     }
 
-    public LocalDate getHireDate() {
-        return hireDate;
+    public LocalDate getHiredDate() {
+        return hiredDate;
     }
 
-    public void setHireDate(LocalDate hireDate) {
-        this.hireDate = hireDate;
+    public void setHiredDate(LocalDate hiredDate) {
+        this.hiredDate = hiredDate;
     }
 
     public LocalDate getTerminationDate() {
@@ -147,4 +228,16 @@ public class Employee {
         this.terminationDate = terminationDate;
     }
     
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
 }
