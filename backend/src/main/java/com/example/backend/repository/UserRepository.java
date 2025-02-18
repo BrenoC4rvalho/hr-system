6fd7224f43ea21f23b2d07b7c2081494f370f752
BrenoC4rvalho <bc101004@gmail.com>
2025-02-18 15:39:45 -0300

    feat: add Derived queries.

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.backend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+ 
+    boolean existsByUsername(String username);
+    boolean existsByEmployeeId(Long employeeId);
     
 }
