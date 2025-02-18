b0d144882ff11a0195d5bacf950fc71fe6f56a52
BrenoC4rvalho <bc101004@gmail.com>
2025-02-18 15:41:41 -0300

    feat: add exception handling for UserService.

---
+++ backend/src/main/java/com/example/backend/exception/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.backend.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException() {
+        super("Employee not found");
+    }
+}
