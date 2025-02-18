fdc90daa8260b89e36b97d1092629424d951554f
BrenoC4rvalho <bc101004@gmail.com>
2025-02-18 15:43:38 -0300

    fix: add condition to check if header is null.

---
+++ backend/src/main/java/com/example/backend/security/SecurityUserFilter.java
@@ -0,0 +1,68 @@
+package com.example.backend.security;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Authentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.backend.security.service.TokenService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityUserFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
+    public SecurityUserFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if(header != null && header.startsWith("Bearer ")) {
+            
+            try {
+                
+                String token = header.substring(7);
+
+                DecodedJWT tokenDecoded = tokenService.validateToken(token);
+
+                Long userId = Long.parseLong(tokenDecoded.getSubject());
+                String role = tokenDecoded.getClaim("role").asString();
+
+                request.setAttribute("user_id", userId);
+                request.setAttribute("user_role", role);
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    userId, 
+                    null, 
+                    List.of(new SimpleGrantedAuthority(role))
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                
+            } catch (Exception e) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token or expired.");
+                return;
+            }
+            
+        }
+
+        
+        filterChain.doFilter(request, response);
+    }
+    
+}
