c457886178ad9fba92ef847d88640984a5d127a0
BrenoC4rvalho <bc101004@gmail.com>
2025-02-19 16:03:37 -0300

    feat: add method FindByName in repository.

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.example.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.backend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
  
+    Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmployeeId(Long employeeId);
     
