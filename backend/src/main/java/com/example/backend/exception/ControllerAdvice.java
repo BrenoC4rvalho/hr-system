4856b08ff0947ddfee608be1a3affe934062e024
BrenoC4rvalho <bc101004@gmail.com>
2025-02-19 15:58:58 -0300

    feat: Add all exceptions to Global Exception Handler.
    - Rename ControllerAdvice - GlobalExceptionHandler.

--- backend/src/main/java/com/example/backend/exception/ControllerAdvice.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.exception;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class ControllerAdvice {
-    
-}
---
+++ backend/src/main/java/com/example/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,85 @@
+package com.example.backend.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Validation Exceptions Handler
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, Object>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Invalid Request.");
+        response.put("message", "The request body is missing or improperly formatted.");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    // Authentication Exceptions Handler
+
+    @ExceptionHandler(AuthFailedException.class)
+    public ResponseEntity<String> handleAuthFailed(AuthFailedException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+    @ExceptionHandler(JWTVerificationException.class)
+    public ResponseEntity<String> handleJwtVerificationError(JWTVerificationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<Map<String, Object>> handleAuthorizationDeniedException(AuthorizationDeniedException e) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Access Denied");
+        response.put("message", "You don't have permission to access this resource.");
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+    }
+
+    // Generics Exceptions Handler
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    // User Exceptions Handler
+
+    // Employee Exceptions Handler
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ResponseEntity<String> handleEmployeeNotFound(EmployeeNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    // Generic exception handler for other, unaddressed errors
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception e) {
+        System.out.println(e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error internal server error .");
+    }
+    
+}
