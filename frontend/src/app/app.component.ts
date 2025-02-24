cd2e70efb25825849584697da477d04c858d9f95
BrenoC4rvalho <bc101004@gmail.com>
2025-02-23 22:08:17 -0300

    feat: update route and settings for auth to work.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ServerErrorComponent } from "./pages/error/serverError/serverError.component";
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './auth/login/login.component';
 import { NotFoundComponent } from './pages/error/notFound/notFound.component';
 import { EmployeesComponent } from './pages/employees/employees.component';
+import { AuthGuard } from './auth/guard/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'employees', component: EmployeesComponent },
+  { path: 'employees', component: EmployeesComponent, canActivate: [AuthGuard] },
   { path: '**', component: NotFoundComponent },
 ];
