9ed8f93c35326e95f29245a25c4c4c4fdb3893af
BrenoC4rvalho <bc101004@gmail.com>
2025-02-24 20:15:00 -0300

    feat: add existsByRole with userRepository.

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.backend.model.User;
+import com.example.backend.enums.UserRole;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
  
@@ -14,5 +16,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmployeeId(Long employeeId);
+    boolean existsByRole(UserRole role);
     
 }
