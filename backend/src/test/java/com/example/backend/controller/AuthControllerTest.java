dbb819721a3c71e8ebacf3b6cb885c3932c268d6
BrenoC4rvalho <bc101004@gmail.com>
2025-02-25 22:32:51 -0300

    test: create authControler test base.

---
+++ backend/src/test/java/com/example/backend/controller/AuthControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.backend.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AuthControllerTest {
+    @DisplayName("test login success.")
+    @Test
+    void testLogin() {
+
+    }
+
+    @DisplayName("test login failure with invalid credentials.")
+    @Test
+    void testLoginFailure() {
+
+    }
+    
+}
