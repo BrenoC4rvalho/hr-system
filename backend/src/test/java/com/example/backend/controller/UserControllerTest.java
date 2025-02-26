f2ee181bbff5af11fa6b54f71eacbdac05912c9c
BrenoC4rvalho <bc101004@gmail.com>
2025-02-26 19:21:26 -0300

    generate file about user controller test

---
+++ backend/src/test/java/com/example/backend/controller/UserControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class UserControllerTest {
+    
+    @Test
+    void testIndex() {
+
+    }
+    
+    @Test
+    void testCreate() {
+
+    }
+
+    @Test
+    void testShow() {
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+
+    @Test
+    void testUpdatePassword() {
+
+    }
+
+    @Test
+    void testDelete() {
+
+    }
+}
