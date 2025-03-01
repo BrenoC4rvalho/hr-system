55689ad59ebcb345fc6d21fcec9a0d7e64769adf
BrenoC4rvalho <bc101004@gmail.com>
2025-03-01 16:36:24 -0300

    feat: add header

--- frontend/src/app/pages/employees/employees.component.ts
+++ frontend/src/app/pages/employees/employees.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ListEmployeesComponent } from "../../components/list-employees/list-employees.component";
 import { EmployeeProfileComponent } from "../../components/employee-profile/employee-profile.component";
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-employees',
-  imports: [ListEmployeesComponent, EmployeeProfileComponent],
+  imports: [ListEmployeesComponent, EmployeeProfileComponent, NavbarComponent],
   templateUrl: './employees.component.html',
 })
 export class EmployeesComponent {
