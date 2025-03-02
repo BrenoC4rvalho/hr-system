0a6486443d823f7e451ba8cb5cd7c7c12576a7f4
BrenoC4rvalho <bc101004@gmail.com>
2025-03-02 16:01:25 -0300

    fix: adjustment interceptor for jwt.

--- frontend/src/app/auth/guard/jwt.interceptor.ts
+++ frontend/src/app/auth/guard/jwt.interceptor.ts
@@ -1,22 +1,20 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
 import { AuthService } from "../service/auth.service";
-import { Observable } from "rxjs";
 
-@Injectable()
-export class JwtInterceptor implements HttpInterceptor {
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
 
-  constructor(private authService: AuthService) {}
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.authService.getToken();
-    if(token) {
-      const clonedRequest = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-      return next.handle(clonedRequest);
-    }
-    return next.handle(req);
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+
+  if(token) {
+    const clonedRequest = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(clonedRequest);
   }
+
+  return next(req)
 }
