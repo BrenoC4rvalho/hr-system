1abb1fe51fe67d2b7967355b0fc97b8e4f727987
BrenoC4rvalho <bc101004@gmail.com>
2025-03-09 19:55:12 -0300

    feat: update password logic

--- frontend/src/app/pages/update-password/update-password.component.ts
+++ frontend/src/app/pages/update-password/update-password.component.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Eye, EyeClosed, LucideAngularModule } from 'lucide-angular';
+import { Router } from '@angular/router';
+import { UserService } from '../../core/service/user.service';
+import { ErrorModalComponent } from '../../components/error-modal/error-modal';
 
 @Component({
   selector: 'app-update-password',
-  imports: [ LucideAngularModule, CommonModule, FormsModule ],
+  imports: [ LucideAngularModule, CommonModule, FormsModule, ErrorModalComponent ],
   templateUrl: './update-password.component.html',
 })
 export class UpdatePasswordComponent {
@@ -13,11 +16,33 @@ export class UpdatePasswordComponent {
   readonly EyeIcon = Eye;
   readonly EyeClosedIcon = EyeClosed
 
+  showErrorModal = false;
+  errorMessage: string = '';
+
   password: string = '';
 
   isPasswordVisible: boolean = false;
 
+  constructor(private userService: UserService, private router: Router) {}
+
   togglePasswordVisibility() {
     this.isPasswordVisible =!this.isPasswordVisible;
+  }
+
+  onSubmit(): void {
+    this.userService.updatePassword(1, this.password.trim()).subscribe({
+      next: (response) => {
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        if (error && error.error) {
+          this.showErrorModal = true;
+          this.errorMessage = error.error;
+        } else {
+          this.showErrorModal = true;
+          this.errorMessage = 'An unexpected error occurred. Please try again later.';
+        }
+      }
+  })
   }
 }
