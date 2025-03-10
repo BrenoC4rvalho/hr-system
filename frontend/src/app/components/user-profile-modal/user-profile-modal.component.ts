adf1aa64b4cbff84111e649e6d0c0edc6494e337
BrenoC4rvalho <bc101004@gmail.com>
2025-03-09 22:13:40 -0300

    feat: html and css for edit in user profile.

--- frontend/src/app/components/user-profile-modal/user-profile-modal.component.ts
+++ frontend/src/app/components/user-profile-modal/user-profile-modal.component.ts
@@ -4,10 +4,13 @@ import { UserService } from '../../core/service/user.service';
 import { CircleX, LucideAngularModule } from 'lucide-angular';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { UserRole } from '../../core/enums/user-role.enum';
+import { UserStatus } from '../../core/enums/user-status.enum';
 
 @Component({
   selector: 'app-user-profile-modal',
-  imports: [LucideAngularModule, CommonModule, RouterModule],
+  imports: [LucideAngularModule, CommonModule, RouterModule, FormsModule],
   templateUrl: './user-profile-modal.component.html',
 })
 export class UserProfileModalComponent implements OnChanges {
@@ -19,14 +22,26 @@ export class UserProfileModalComponent implements OnChanges {
   @Output() closed = new EventEmitter<void>()
   @Output() errorUserProfileModal = new EventEmitter<void>()
 
+  isEditing = false;
+
+  editableUser: User = {} as User;
 
   user: User | undefined;
+
+  userRoles: UserRole[] = Object.values(UserRole)
+  userStatus: UserStatus[] = Object.values(UserStatus);
 
   constructor(private userService: UserService) { }
 
   ngOnChanges(changes: SimpleChanges): void {
     if(changes['isVisible'] && this.isVisible) {
       this.getUserProfile()
+    }
+  }
+
+  ngOnInit() {
+    if(this.user) {
+      this.editableUser = { ...this.user };
     }
   }
 
@@ -53,6 +68,22 @@ export class UserProfileModalComponent implements OnChanges {
   onClose() {
     this.isVisible = false;
     this.closed.emit();
+  }
+
+  saveChanges() {
+    // fazer requisicao
+    this.isEditing = false;
+  }
+
+  toggleEdit() {
+    this.isEditing = true;
+  }
+
+  cancelEdit() {
+    if(this.user) {
+      this.editableUser = {...this.user };
+    }
+    this.isEditing = false;
   }
 
 }
