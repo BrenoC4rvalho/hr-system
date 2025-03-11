4fb526598d5202c99e42d55fe25903ae5276368d
BrenoC4rvalho <bc101004@gmail.com>
2025-03-11 12:25:30 -0300

    feat: create methods in Employee Controller.

--- backend/src/main/java/com/example/backend/controller/EmployeeController.java
+++ backend/src/main/java/com/example/backend/controller/EmployeeController.java
@@ -1,7 +1,16 @@
 package com.example.backend.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.backend.service.EmployeeService;
 
+@RestController
+@RequestMapping("/employees")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -9,5 +18,30 @@ public class EmployeeController {
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-    
+
+    @GetMapping() 
+    public void index() {
+
+    }
+
+    @PostMapping()
+    public void create() {
+
+    }
+
+    @GetMapping("/{id}")
+    public void show() {
+
+    }
+
+    @PutMapping("/{id}")
+    public void update() {
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete() {
+        
+    }
+
 }
