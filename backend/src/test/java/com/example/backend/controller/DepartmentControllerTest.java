33e24628309a74d9dea6239bc89bb0d19c4cd348
BrenoC4rvalho <bc101004@gmail.com>
2025-03-19 23:02:11 -0300

    test: deparment controller create.

--- backend/src/test/java/com/example/backend/controller/DepartmentControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/DepartmentControllerTest.java
@@ -1,13 +1,24 @@
 package com.example.backend.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.example.backend.dto.AuthRequestDTO;
 import com.example.backend.dto.AuthResponseDTO;
+import com.example.backend.dto.CreateDepartmentDTO;
+import com.example.backend.dto.DepartmentDTO;
 import com.example.backend.service.UserService;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,4 +36,23 @@ public class DepartmentControllerTest {
             "/auth", request, AuthResponseDTO.class);
         return response.getBody().getToken();
     }
+
+    @DisplayName("Test create department.")
+    @Test
+    void shouldCreateDepartment() {
+
+        String token = getAuthToken("admin", "password");
+
+        CreateDepartmentDTO request = new CreateDepartmentDTO("HR");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<CreateDepartmentDTO> entity = new HttpEntity<>(request, headers);
+        
+        ResponseEntity<DepartmentDTO> response = restTemplate.postForEntity("/departments", entity, DepartmentDTO.class);
+        
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+    }
+
 }
