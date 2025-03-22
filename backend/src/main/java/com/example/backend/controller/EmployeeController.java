19362ea3668a07d20434d0ecd3b6e0b3b5a956d2
BrenoC4rvalho <bc101004@gmail.com>
2025-03-21 22:31:30 -0300

    feat: add create employee controller and service,

--- backend/src/main/java/com/example/backend/controller/EmployeeController.java
+++ backend/src/main/java/com/example/backend/controller/EmployeeController.java
@@ -1,13 +1,20 @@
 package com.example.backend.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.backend.dto.CreateEmployeeDTO;
+import com.example.backend.dto.EmployeeDTO;
 import com.example.backend.service.EmployeeService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/employees")
@@ -25,8 +32,9 @@ public class EmployeeController {
     }
 
     @PostMapping()
-    public void create() {
-
+    public ResponseEntity<?> create(@Valid @RequestBody CreateEmployeeDTO createEmployeeDTO) {
+        EmployeeDTO newEmployee = employeeService.create(createEmployeeDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newEmployee);
     }
 
     @GetMapping("/{id}")
--- backend/src/main/java/com/example/backend/service/EmployeeService.java
+++ backend/src/main/java/com/example/backend/service/EmployeeService.java
@@ -1,15 +1,32 @@
 package com.example.backend.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.backend.dto.CreateEmployeeDTO;
+import com.example.backend.dto.EmployeeDTO;
+import com.example.backend.mapper.CreateEmployeeMapper;
+import com.example.backend.mapper.EmployeeMapper;
+import com.example.backend.model.Employee;
 import com.example.backend.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
-    
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    private final CreateEmployeeMapper createEmployeeMapper;
+    private final EmployeeMapper employeeMapper;
+
+    public EmployeeService(EmployeeRepository employeeRepository, CreateEmployeeMapper createEmployeeMapper, EmployeeMapper employeeMapper) {
         this.employeeRepository = employeeRepository;
+        this.createEmployeeMapper = createEmployeeMapper;
+        this.employeeMapper = employeeMapper;
+    }
+
+    @Transactional
+    public EmployeeDTO create(CreateEmployeeDTO createEmployeeDTO) {
+        Employee newEmployee = createEmployeeMapper.map(createEmployeeDTO);
+        Employee savedEmployee = employeeRepository.save(newEmployee);
+        return employeeMapper.map(savedEmployee);
     }
 }
