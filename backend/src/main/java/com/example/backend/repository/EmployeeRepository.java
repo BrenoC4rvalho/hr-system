3f2a9d7b474e2045fb8f215b085ad40f854d52f4
BrenoC4rvalho <bc101004@gmail.com>
2025-03-23 17:16:05 -0300

    test: get all employee

--- backend/src/main/java/com/example/backend/repository/EmployeeRepository.java
+++ backend/src/main/java/com/example/backend/repository/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.example.backend.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.backend.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
   
+    Page<Employee> findAll(Pageable pageable);
 } 
--- backend/src/test/java/com/example/backend/service/EmployeeServiceTest.java
+++ backend/src/test/java/com/example/backend/service/EmployeeServiceTest.java
@@ -1,11 +1,25 @@
 package com.example.backend.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.example.backend.dto.CreateEmployeeDTO;
 import com.example.backend.dto.EmployeeDTO;
@@ -53,4 +67,18 @@ public class EmployeeServiceTest {
         createEmployeeDTO.setLastName("Doe");
     }
 
+    @DisplayName("Test get all employees.")
+    @Test
+    void shouldReturnAllEmployees() {
+        Pageable pageable = PageRequest.of(0, 10);
+        List<Employee> employeeList = List.of(employee);
+        Page<Employee> employeePage = new PageImpl<>(employeeList, pageable, employeeList.size());
+    
+        when(employeeRepository.findAll(pageable)).thenReturn(employeePage);
+    
+        Page<EmployeeDTO> result = employeeService.getAll(pageable);
+    
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.getTotalElements());
+    }
 }
