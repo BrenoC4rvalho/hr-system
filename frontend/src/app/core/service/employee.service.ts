1c981ab9f07dae364009cd13062e856ab3d6b1e4
BrenoC4rvalho <bc101004@gmail.com>
2025-03-25 22:53:04 -0300

    feat: employee service in frontend implements get all and show.

--- frontend/src/app/core/service/employee.service.ts
+++ frontend/src/app/core/service/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,15 @@ export class EmployeeService {
   private apiUrl = `${environment.apiUrl}/employees`;
 
   constructor(private http: HttpClient) { }
+
+    getAll(page: number, size: number): Observable<any[]> {
+      const params = new HttpParams()
+       .set('page', page.toString())
+       .set('size', size.toString());
+      return this.http.get<any[]>(this.apiUrl, { params });
+    }
+
+    show(id: number): Observable<any> {
+      return this.http.get<any>(`${this.apiUrl}/${id}`);
+    }
 }
