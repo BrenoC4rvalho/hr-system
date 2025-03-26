09c00d1a57c150bf833523be9419a2f3bae69162
BrenoC4rvalho <bc101004@gmail.com>
2025-03-25 22:52:41 -0300

    feat: department service in frontend implements get all and show.

--- frontend/src/app/core/service/department.service.ts
+++ frontend/src/app/core/service/department.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environments';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,16 @@ export class DepartmentService {
   private apiUrl = `${environment.apiUrl}/departments`
 
   constructor(private http: HttpClient) { }
+
+    getAll(page: number, size: number): Observable<any[]> {
+      const params = new HttpParams()
+       .set('page', page.toString())
+       .set('size', size.toString());
+      return this.http.get<any[]>(this.apiUrl, { params });
+    }
+
+    show(id: number): Observable<any> {
+      return this.http.get<any>(`${this.apiUrl}/${id}`);
+    }
 
 }
