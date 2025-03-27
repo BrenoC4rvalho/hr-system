8fd4bbace6973e250025aab685102d828a984e0f
BrenoC4rvalho <bc101004@gmail.com>
2025-03-26 23:22:32 -0300

    feat: employee model.

---
+++ frontend/src/app/core/model/employee.ts
@@ -0,0 +1,23 @@
+import { EmployeeStatus } from "../enums/employee-status.enum";
+import { Gender } from "../enums/gender.enum";
+import { Shift } from "../enums/shift.enum";
+import { Department } from "./department";
+import { Position } from "./position";
+
+export interface Employee {
+
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  gender: Gender,
+  department: Department;
+  position: Position;
+  shift: Shift;
+  Status: EmployeeStatus;
+  birthDate: Date;
+  hiredDate: Date;
+  terminationDate: Date;
+
+}
