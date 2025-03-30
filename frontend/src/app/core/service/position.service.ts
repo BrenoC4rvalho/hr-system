9e3584021f05eb5e671feef546b03beffe7d8ac7
BrenoC4rvalho <bc101004@gmail.com>
2025-03-29 23:14:34 -0300

    feat: create method update and create im position service.

--- frontend/src/app/core/service/position.service.ts
+++ frontend/src/app/core/service/position.service.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environments';
 import { Observable } from 'rxjs';
+import { CreatePosition } from '../model/create-position';
+import { Position } from '../model/position';
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +14,23 @@ export class PositionService {
 
   constructor(private http: HttpClient) { }
 
-  getAll(page: number, size: number): Observable<any[]> {
+  getAll(page: number, size: number): Observable<Position[]> {
     const params = new HttpParams()
      .set('page', page.toString())
      .set('size', size.toString());
-    return this.http.get<any[]>(this.apiUrl, { params });
+    return this.http.get<Position[]>(this.apiUrl, { params });
   }
 
-  show(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  show(id: number): Observable<Position> {
+    return this.http.get<Position>(`${this.apiUrl}/${id}`);
+  }
+
+  create(body: CreatePosition): Observable<Position> {
+    return this.http.post<Position>(this.apiUrl, body);
+  }
+
+  update(id: number, body: Position): Observable<Position> {
+    return this.http.put<Position>(`${this.apiUrl}/${id}`, body);
   }
 
 }
