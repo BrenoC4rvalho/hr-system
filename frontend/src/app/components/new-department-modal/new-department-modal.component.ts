bae0247653235ba159d5fad8bec23477092ecbf4
BrenoC4rvalho <bc101004@gmail.com>
2025-04-14 22:28:20 -0300

    feat: logic for new department modal.

--- frontend/src/app/components/new-department-modal/new-department-modal.component.ts
+++ frontend/src/app/components/new-department-modal/new-department-modal.component.ts
@@ -3,6 +3,7 @@ import { CircleX, LucideAngularModule } from 'lucide-angular';
 import { DepartmentService } from '../../core/service/department.service';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Department } from '../../core/model/department';
 
 @Component({
   selector: 'app-new-department-modal',
@@ -14,13 +15,22 @@ export class NewDepartmentModalComponent {
   readonly CircleXIcon = CircleX;
 
   @Output() closeModal = new EventEmitter<void>()
+  @Output() createdDepartment = new EventEmitter<Department>()
 
   name: string = ''
 
   constructor(private departmentService: DepartmentService) {}
 
   onSubmit(): void {
+    this.departmentService.create({ name: this.name }).subscribe({
+      next: (response: Department) => {
+        this.createdDepartment.emit(response);
+        this.closeModal.emit();
+      },
+      error: (error) => {
 
+      }
+    })
   }
 
   onClose() {
