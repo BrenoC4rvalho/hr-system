9d1b1f98d3826f1ac919d49c241624d1a815cc1d
BrenoC4rvalho <bc101004@gmail.com>
2025-04-14 22:22:52 -0300

    feat: html new department modal

--- frontend/src/app/components/new-department-modal/new-department-modal.component.ts
+++ frontend/src/app/components/new-department-modal/new-department-modal.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CircleX, LucideAngularModule } from 'lucide-angular';
 import { DepartmentService } from '../../core/service/department.service';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-department-modal',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, FormsModule, CommonModule],
   templateUrl: './new-department-modal.component.html',
 })
 export class NewDepartmentModalComponent {
@@ -13,7 +15,13 @@ export class NewDepartmentModalComponent {
 
   @Output() closeModal = new EventEmitter<void>()
 
+  name: string = ''
+
   constructor(private departmentService: DepartmentService) {}
+
+  onSubmit(): void {
+
+  }
 
   onClose() {
     this.closeModal.emit();
