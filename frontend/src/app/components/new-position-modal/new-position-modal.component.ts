fc14bee2dec27b2ff41aced097f3c412d67f6fe6
BrenoC4rvalho <bc101004@gmail.com>
2025-04-15 22:20:35 -0300

    feat: html for new position modal.

---
+++ frontend/src/app/components/new-position-modal/new-position-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-position-modal',
+  imports: [],
+  templateUrl: './new-position-modal.component.html',
+  styleUrl: './new-position-modal.component.css'
+})
+export class NewPositionModalComponent {
+
+}
