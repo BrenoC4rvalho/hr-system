130d09421874f0bd8ab7374065e8f3f8bedf6b45
BrenoC4rvalho <bc101004@gmail.com>
2025-04-15 22:26:57 -0300

    feat: create logic for new position modal.

--- frontend/src/app/components/new-position-modal/new-position-modal.component.ts
+++ frontend/src/app/components/new-position-modal/new-position-modal.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CircleX, LucideAngularModule } from 'lucide-angular';
+import { Position } from '../../core/model/position';
+import { PositionService } from '../../core/service/position.service';
 
 @Component({
   selector: 'app-new-position-modal',
-  imports: [],
+  imports: [CommonModule, FormsModule, LucideAngularModule],
   templateUrl: './new-position-modal.component.html',
-  styleUrl: './new-position-modal.component.css'
 })
 export class NewPositionModalComponent {
+
+  readonly CircleXIcon = CircleX;
+
+  name: string = '';
+  description: string = '';
+
+  @Output() closeModal = new EventEmitter<void>()
+  @Output() createdPosition = new EventEmitter<Position>()
+
+  constructor(private positionService: PositionService) {}
+
+  onClose(): void {
+    this.closeModal.emit();
+  }
+
+  onSubmit(): void {
+    this.positionService.create({ name: this.name, description: this.description }).subscribe({
+          next: (response: Position) => {
+            this.createdPosition.emit(response);
+            this.closeModal.emit();
+          },
+          error: (error) => {
+
+          }
+        })
+  }
+
 
 }
