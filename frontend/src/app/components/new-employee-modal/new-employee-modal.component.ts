f2763d42d727ecab041c8a09beaa3aed0e7ae170
BrenoC4rvalho <bc101004@gmail.com>
2025-04-16 22:19:19 -0300

    feat: create new employee modal.

---
+++ frontend/src/app/components/new-employee-modal/new-employee-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-employee-modal',
+  imports: [],
+  templateUrl: './new-employee-modal.component.html',
+  styleUrl: './new-employee-modal.component.css'
+})
+export class NewEmployeeModalComponent {
+
+}
