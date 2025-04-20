0a068b43fe12f1fa862097bc1531d10b852baff7
BrenoC4rvalho <bc101004@gmail.com>
2025-04-19 23:34:44 -0300

    feat: generate list birthday

---
+++ frontend/src/app/component/list-birthday/list-birthday.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-birthday',
+  imports: [],
+  templateUrl: './list-birthday.component.html',
+  styleUrl: './list-birthday.component.css'
+})
+export class ListBirthdayComponent {
+
+}
