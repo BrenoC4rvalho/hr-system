3194bfeefe26a36390a7dec6c0a0bdf23111fef9
BrenoC4rvalho <bc101004@gmail.com>
2025-04-20 22:14:20 -0300

    feat: add type in methods.

--- frontend/src/app/core/service/employee.service.ts
+++ frontend/src/app/core/service/employee.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CreateEmployee } from '../model/create-employee';
 import { Employee } from '../model/employee';
+import { PaginatedEmployeesResponse } from '../model/paginated-employees-response';
 
 @Injectable({
   providedIn: 'root'
@@ -14,15 +15,15 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
-    getAll(page: number, size: number): Observable<any[]> {
+    getAll(page: number, size: number): Observable<PaginatedEmployeesResponse[]> {
       const params = new HttpParams()
        .set('page', page.toString())
        .set('size', size.toString());
-      return this.http.get<any[]>(this.apiUrl, { params });
+      return this.http.get<PaginatedEmployeesResponse[]>(this.apiUrl, { params });
     }
 
-    show(id: number): Observable<any> {
-      return this.http.get<any>(`${this.apiUrl}/${id}`);
+    show(id: number): Observable<Employee> {
+      return this.http.get<Employee>(`${this.apiUrl}/${id}`);
     }
 
     create(body: CreateEmployee): Observable<Employee> {
