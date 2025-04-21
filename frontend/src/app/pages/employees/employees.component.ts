f832dc98c1cf72ac3a78a99bc6cf542957b9d89b
BrenoC4rvalho <bc101004@gmail.com>
2025-04-20 23:52:13 -0300

    feat: add new employee modal in page employee,

--- frontend/src/app/pages/employees/employees.component.ts
+++ frontend/src/app/pages/employees/employees.component.ts
@@ -2,14 +2,32 @@ import { Component } from '@angular/core';
 import { ListEmployeesComponent } from "../../components/list-employees/list-employees.component";
 import { EmployeeProfileComponent } from "../../components/employee-profile/employee-profile.component";
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { Employee } from '../../core/model/employee';
+import { NewEmployeeModalComponent } from "../../components/new-employee-modal/new-employee-modal.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employees',
-  imports: [ListEmployeesComponent, NavbarComponent],
+  imports: [ListEmployeesComponent, NavbarComponent, NewEmployeeModalComponent, CommonModule],
   templateUrl: './employees.component.html',
 })
 export class EmployeesComponent {
 
+  newEmployee: Employee | undefined;
+
+  isModalNewEmployeeOpen: boolean = false;
+
+  openModalNewEmployee() {
+    this.isModalNewEmployeeOpen = true;
+  }
+
+  closeModalNewEmployee() {
+    this.isModalNewEmployeeOpen = false;
+  }
+
+  onEmployeeCreated($event: Employee) {
+    this.newEmployee = $event;
+  }
 
 
 }
