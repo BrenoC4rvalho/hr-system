1fa117ee2cd6a17c79e85a9cb6d526b6038a7a2b
BrenoC4rvalho <bc101004@gmail.com>
2025-04-22 18:51:29 -0300

    feat: create dynamic field

---
+++ frontend/src/app/components/dynamic-field/dynamic-field.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dynamic-field',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './dynamic-field.component.html',
+})
+export class DynamicFieldComponent {
+
+  @Input() field: any;
+  @Input() form!: FormGroup;
+
+  get control() {
+    return this.form.get(this.field.name);
+  }
+
+}
+
