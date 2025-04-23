7ed0f7c3359a1a6c97a0e7756852852853fabd5f
BrenoC4rvalho <bc101004@gmail.com>
2025-04-23 20:59:53 -0300

    feat: git add reactive forms module

--- frontend/src/app/components/new-department-modal/new-department-modal.component.ts
+++ frontend/src/app/components/new-department-modal/new-department-modal.component.ts
@@ -1,13 +1,15 @@
 import { Component, EventEmitter, output, Output } from '@angular/core';
 import { CircleX, LucideAngularModule } from 'lucide-angular';
 import { DepartmentService } from '../../core/service/department.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Department } from '../../core/model/department';
+import { ModalComponent } from "../../shared/modal/modal.component";
+import { CreateDepartment } from '../../core/model/create-department';
 
 @Component({
   selector: 'app-new-department-modal',
-  imports: [LucideAngularModule, FormsModule, CommonModule],
+  imports: [LucideAngularModule, FormsModule, CommonModule, ModalComponent],
   templateUrl: './new-department-modal.component.html',
 })
 export class NewDepartmentModalComponent {
@@ -18,12 +20,27 @@ export class NewDepartmentModalComponent {
   @Output() createdDepartment = new EventEmitter<Department>()
   @Output() errorMessage = new EventEmitter<string>()
 
-  name: string = ''
+  form: FormGroup;
 
-  constructor(private departmentService: DepartmentService) {}
+  constructor(
+    private fb: FormBuilder,
+    private departmentService: DepartmentService
+  ) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]]
+    })
+  }
 
-  onSubmit(): void {
-    this.departmentService.create({ name: this.name }).subscribe({
+  onSave(): void {
+
+    if(this.form.invalid) {
+      this.errorMessage.emit('Fill in all required fields.')
+      return
+    }
+
+    const newDepartment: CreateDepartment = this.form.value;
+
+    this.departmentService.create(newDepartment).subscribe({
       next: (response: Department) => {
         this.createdDepartment.emit(response);
         this.closeModal.emit();
