db5dd5d5399f5f68633ba574bcc57563cc5750a6
BrenoC4rvalho <bc101004@gmail.com>
2025-04-22 22:34:36 -0300

    feat: edit tailwind dynamic field

--- frontend/src/app/components/dynamic-field/dynamic-field.component.ts
+++ frontend/src/app/components/dynamic-field/dynamic-field.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Field } from '../../core/types/Field';
 
 @Component({
   selector: 'app-dynamic-field',
