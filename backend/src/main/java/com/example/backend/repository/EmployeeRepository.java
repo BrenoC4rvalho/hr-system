eed56531d6e444e237dcbbdb9cb5a79c494e3ada
BrenoC4rvalho <bc101004@gmail.com>
2025-04-24 00:02:53 -0300

    feat: employee repository

--- backend/src/main/java/com/example/backend/repository/EmployeeRepository.java
+++ backend/src/main/java/com/example/backend/repository/EmployeeRepository.java
@@ -1,12 +1,9 @@
 package com.example.backend.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.example.backend.model.Employee;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-  
-    Page<Employee> findAll(Pageable pageable);
+public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
 } 
