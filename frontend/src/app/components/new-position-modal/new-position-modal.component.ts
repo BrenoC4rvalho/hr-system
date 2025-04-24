d5b146b23abfa0ce5633c7dc70f8c3b60200a1d2
BrenoC4rvalho <bc101004@gmail.com>
2025-04-24 00:01:24 -0300

    feat: input group in new position modal.

--- frontend/src/app/components/new-position-modal/new-position-modal.component.ts
+++ frontend/src/app/components/new-position-modal/new-position-modal.component.ts
@@ -6,10 +6,12 @@ import { Position } from '../../core/model/position';
 import { PositionService } from '../../core/service/position.service';
 import { ModalComponent } from "../../shared/modal/modal.component";
 import { CreatePosition } from '../../core/model/create-position';
+import { Field } from '../../core/types/Field';
+import { DynamicFieldComponent } from "../dynamic-field/dynamic-field.component";
 
 @Component({
   selector: 'app-new-position-modal',
-  imports: [CommonModule, LucideAngularModule, ModalComponent, ReactiveFormsModule],
+  imports: [CommonModule, LucideAngularModule, ModalComponent, ReactiveFormsModule, DynamicFieldComponent],
   templateUrl: './new-position-modal.component.html',
 })
 export class NewPositionModalComponent {
@@ -20,13 +22,20 @@ export class NewPositionModalComponent {
 
   form: FormGroup;
 
+  inputGroups: Field[][] = [
+    [
+      { label: 'Name', name: 'name', type: 'input', inputType: 'text', placeholder: 'Maintenance technician' },
+      { label: 'Description', name: 'description', type: 'input', inputType: 'textarea', placeholder: 'specialized maintenance function' }
+    ]
+  ]
+
   constructor(
     private fb: FormBuilder,
     private positionService: PositionService
   ) {
     this.form = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
-      description: [Validators.required, Validators.minLength(5), Validators.maxLength(255)]
+      description: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(255)]]
     })
   }
 
