e6a74a4d5d4e149a4d9fd52191cf57dbf990bbb3
BrenoC4rvalho <bc101004@gmail.com>
2025-04-27 22:44:42 -0300

    feat: style for department card.

--- frontend/src/app/components/department-card/department-card.component.ts
+++ frontend/src/app/components/department-card/department-card.component.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Department } from '../../core/model/department';
 import { DepartmentService } from '../../core/service/department.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-department-card',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './department-card.component.html',
 })
 export class DepartmentCardComponent implements OnInit{
@@ -14,18 +15,21 @@ export class DepartmentCardComponent implements OnInit{
 
   departments: Department[] = []
   totalDepartments: number = 0;
+  totalEmployees: number = 0;
 
   constructor(private departmentService: DepartmentService) {}
 
   ngOnInit(): void {
     this.getDepartments();
-    this.totalDepartments = this.departments.length;
   }
 
   getDepartments(): void {
     this.departmentService.getAll().subscribe({
       next: (response: Department[]) => {
         this.departments = response;
+        this.totalDepartments = this.departments.length;
+        this.totalEmployees = response.reduce((sum, department) => sum + department.numberOfEmployees, 0);
+        console.log(this.totalEmployees)
       },
       error: (error) => {
         if(error && error.error) {
