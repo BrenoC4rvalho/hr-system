d96a28aca0c2544e7e9e14c309ba6edbcacb2f57
BrenoC4rvalho <bc101004@gmail.com>
2025-04-27 22:48:00 -0300

    feat: error modal in dashboard.

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { ListBirthdayComponent } from "../../components/list-birthday/list-birthday.component";
 import { DepartmentCardComponent } from "../../components/department-card/department-card.component";
+import { ErrorModalComponent } from "../../components/error-modal/error-modal";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [NavbarComponent, ListBirthdayComponent, DepartmentCardComponent],
+  imports: [NavbarComponent, ListBirthdayComponent, DepartmentCardComponent, ErrorModalComponent],
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
+
+  showErrorModal: boolean = false;
+  errorMessage: string = ''
+
+  handleErrorModal($event: string) {
+    this.showErrorModal = true;
+    this.errorMessage = $event;
+  }
 
 }
