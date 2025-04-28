bab2f899eb49e5b68078bb779f6e4f17222a55e9
BrenoC4rvalho <bc101004@gmail.com>
2025-04-27 21:47:02 -0300

    fix: update list-birthday component html

--- frontend/src/app/components/list-birthday/list-birthday.component.ts
+++ frontend/src/app/components/list-birthday/list-birthday.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LucideAngularModule, UserSearch } from 'lucide-angular';
 
 @Component({
   selector: 'app-list-birthday',
-  imports: [],
+  imports: [FormsModule, CommonModule, LucideAngularModule],
   templateUrl: './list-birthday.component.html',
-  styleUrl: './list-birthday.component.css'
 })
 export class ListBirthdayComponent {
+
+  readonly UserRoundSearchIcon = UserSearch;
+
+  searchText: string = '';
+  users = [
+    { name: 'Edwards Mckenz', role: 'Chief Financial Officer (CFO)', avatar: 'https://via.placeholder.com/40' },
+    { name: 'Daisy Wilson', role: 'Director of Finance', avatar: 'https://via.placeholder.com/40' },
+    { name: 'Bianca Anderson', role: 'Financial Analyst', avatar: 'https://via.placeholder.com/40' },
+    { name: 'Laurent Perrier', role: 'Finance Assistant', avatar: 'https://via.placeholder.com/40' },
+    { name: 'Nunez Faulkner', role: 'Budget Analyst', avatar: 'https://via.placeholder.com/40' },
+    { name: 'Gilbert Barrett', role: 'Financial Analyst', avatar: 'https://via.placeholder.com/40' }
+  ];
+
+  get filteredUsers() {
+    return this.users.filter(u =>
+      u.name.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
 
 }
