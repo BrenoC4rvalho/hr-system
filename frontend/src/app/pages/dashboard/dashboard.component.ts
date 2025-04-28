4ec07398ed5d4cdda0189d2336dc0b9d086320bd
BrenoC4rvalho <bc101004@gmail.com>
2025-04-27 21:47:27 -0300

    feat: add list birthday in dashboard.

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
+import { ListBirthdayComponent } from "../../components/list-birthday/list-birthday.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ListBirthdayComponent],
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
