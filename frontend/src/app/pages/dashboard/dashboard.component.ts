6586dc742060470b0afbf7d0d23d2f7866c11e0a
BrenoC4rvalho <bc101004@gmail.com>
2025-04-27 22:18:38 -0300

    feat: add departmet card in dashboard.

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import { ListBirthdayComponent } from "../../components/list-birthday/list-birthday.component";
+import { DepartmentCardComponent } from "../../components/department-card/department-card.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [NavbarComponent, ListBirthdayComponent],
+  imports: [NavbarComponent, ListBirthdayComponent, DepartmentCardComponent],
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
