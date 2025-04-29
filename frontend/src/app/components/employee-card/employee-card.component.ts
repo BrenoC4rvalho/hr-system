1b8c54a947c0c5ab43cd1f828cbf950e99800e43
BrenoC4rvalho <bc101004@gmail.com>
2025-04-28 22:53:07 -0300

    feat: create employee card.

---
+++ frontend/src/app/components/employee-card/employee-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-card',
+  imports: [],
+  templateUrl: './employee-card.component.html',
+})
+export class EmployeeCardComponent {
+
+  totalEmployees: number = 0;
+
+}
