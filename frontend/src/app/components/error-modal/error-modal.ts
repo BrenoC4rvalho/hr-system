93ca1fe0945de1af8a8a479fe90c943a1eea99fa
BrenoC4rvalho <bc101004@gmail.com>
2025-05-03 13:29:10 -0300

    feat(error-modal): implement fade-out transition and top progress bar for auto-dismiss.

--- frontend/src/app/components/error-modal/error-modal.ts
+++ frontend/src/app/components/error-modal/error-modal.ts
@@ -1,13 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { AlertCircle, LucideAngularModule, XCircle } from 'lucide-angular';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-error-modal',
   imports: [LucideAngularModule, CommonModule],
   templateUrl: './error-modal.component.html',
+  animations: [
+    trigger('fadeAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('200ms ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('400ms ease-out', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
-export class ErrorModalComponent {
+export class ErrorModalComponent implements OnChanges {
 
   readonly XCircleIcon = XCircle;
   readonly AlertCircleIcon = AlertCircle;
@@ -16,9 +28,27 @@ export class ErrorModalComponent {
   @Input() errorMessage: string = '';
   @Output() closed = new EventEmitter<void>();
 
+  autoCloseDelay = 10000;
+  timeoutRef: any;
+  visibleState: string = 'visible';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['isVisible'] && this.isVisible === true) {
+      clearTimeout(this.timeoutRef);
+      this.visibleState = 'visible';
+      this.closeModalAfterDelay(this.autoCloseDelay);
+    }
+  }
+
   close(): void {
     this.isVisible = false;
     this.closed.emit();
+  }
+
+  closeModalAfterDelay(delay: number = 10000): void {
+    this.timeoutRef = setTimeout(() => {
+      this.close();
+    }, delay);
   }
 
 }
