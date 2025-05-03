883bdbf1be54750e5f86e013f3bf0c9d7e7e3723
BrenoC4rvalho <bc101004@gmail.com>
2025-05-02 23:08:20 -0300

    feat: implement list employee search

---
+++ frontend/src/app/components/list-employee-search/list-employee-search.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { EmployeeBasic } from '../../core/model/employee-basic';
+import { EmployeeService } from '../../core/service/employee.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list-employee-search',
+  imports: [ CommonModule ],
+  templateUrl: './list-employee-search.component.html',
+})
+export class ListEmployeeSearchComponent implements OnChanges {
+
+  @Input() firstName: string = '';
+  @Input() departmentId?: number;
+  @Output() employeeSelected = new EventEmitter<EmployeeBasic>();
+  @Output() errorMessage = new EventEmitter<string>();
+
+  employees: EmployeeBasic[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['firstName'] || changes['departmentId']) {
+      this.getEmployees(this.firstName, this.departmentId);
+    }
+  }
+
+
+  getEmployees(firstName: string, departmentId?: number): void {
+    this.employeeService.getEmployeeByName(firstName, departmentId).subscribe({
+      next: (response: EmployeeBasic[]) => {
+        this.employees = response;
+      },
+      error: (error) => {
+        if(error && error.error) {
+          this.errorMessage.emit(error.error);
+        } else {
+          this.errorMessage.emit('An unexpected error occurred. Please try again later.');
+        }
+      }
+    })
+  }
+
+}
