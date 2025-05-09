ea82808f313de37444c6f38c80b3a98ab8d8aa17
BrenoC4rvalho <bc101004@gmail.com>
2025-05-08 22:48:55 -0300

    feat: edit department in department profile modal.

--- frontend/src/app/components/department-profile-modal/department-profile-modal.component.ts
+++ frontend/src/app/components/department-profile-modal/department-profile-modal.component.ts
@@ -1,21 +1,72 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { ModalComponent } from "../../shared/modal/modal.component";
 import { Department } from '../../core/model/department';
 import { CommonModule } from '@angular/common';
+import { DepartmentService } from '../../core/service/department.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-department-profile-modal',
-  imports: [ModalComponent, CommonModule],
+  imports: [ModalComponent, CommonModule, FormsModule],
   templateUrl: './department-profile-modal.component.html',
 })
-export class DepartmentProfileModalComponent {
+export class DepartmentProfileModalComponent implements OnChanges {
 
-  @Input() department: Department | null = null;
+  @Input() departmentId: number | undefined;
   @Output() closeModal = new EventEmitter<void>();
   @Output() errorMessage = new EventEmitter<string>();
+
+  department: Department | null = null;
+  editableDepartment: Department = {} as Department;
+  isEditing = false;
+
+  constructor(private departmentService: DepartmentService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['departmentId'] && this.departmentId) {
+      this.loadDepartment();
+    }
+  }
+
+  loadDepartment(): void {
+    if (!this.departmentId) return;
+    this.departmentService.show(this.departmentId).subscribe({
+      next: (department: Department) => {
+        this.department = department;
+        this.editableDepartment = { ...department };
+      },
+      error: () => {
+        this.errorMessage.emit('Failed to load department');
+      }
+    });
+  }
+
+  toggleEdit(): void {
+    this.isEditing = true;
+  }
+
+  cancelEdit(): void {
+    if (this.department) {
+      this.editableDepartment = { ...this.department };
+    }
+    this.isEditing = false;
+  }
+
+  saveChanges(): void {
+    if (!this.departmentId) return;
+
+    this.departmentService.update(this.departmentId, this.editableDepartment).subscribe({
+      next: (updatedDept) => {
+        this.department = updatedDept;
+        this.isEditing = false;
+      },
+      error: () => {
+        this.errorMessage.emit('Failed to save changes.');
+      }
+    });
+  }
 
   onClose(): void {
     this.closeModal.emit();
   }
-
 }
