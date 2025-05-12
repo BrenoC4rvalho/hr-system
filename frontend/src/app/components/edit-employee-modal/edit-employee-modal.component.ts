79c9d04683c38f245e2d488233ff853b9d2cd868
BrenoC4rvalho <bc101004@gmail.com>
2025-05-11 22:25:01 -0300

    feat: generate edit employee modal.

---
+++ frontend/src/app/components/edit-employee-modal/edit-employee-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-employee-modal',
+  imports: [],
+  templateUrl: './edit-employee-modal.component.html',
+  styleUrl: './edit-employee-modal.component.css'
+})
+export class EditEmployeeModalComponent {
+
+}
