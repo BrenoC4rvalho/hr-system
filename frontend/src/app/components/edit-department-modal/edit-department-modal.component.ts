fdfbf7dc8fd46339fc0acba7e291759958941a9f
BrenoC4rvalho <bc101004@gmail.com>
2025-05-11 22:24:42 -0300

    feat: generate edit department modal.

---
+++ frontend/src/app/components/edit-department-modal/edit-department-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-department-modal',
+  imports: [],
+  templateUrl: './edit-department-modal.component.html',
+  styleUrl: './edit-department-modal.component.css'
+})
+export class EditDepartmentModalComponent {
+
+}
