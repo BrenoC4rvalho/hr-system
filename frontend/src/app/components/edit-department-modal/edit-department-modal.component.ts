d7450f5d9279730bd19ec2fd4eb45d8c0ea1b51c
BrenoC4rvalho <bc101004@gmail.com>
2025-05-12 22:24:50 -0300

    feat: implement edit department modal.

--- frontend/src/app/components/edit-department-modal/edit-department-modal.component.ts
+++ frontend/src/app/components/edit-department-modal/edit-department-modal.component.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ModalComponent } from "../../shared/modal/modal.component";
+import { Department } from '../../core/model/department';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Field } from '../../core/types/Field';
+import { DepartmentService } from '../../core/service/department.service';
+import { CommonModule } from '@angular/common';
+import { DynamicFieldComponent } from '../dynamic-field/dynamic-field.component';
 
 @Component({
   selector: 'app-edit-department-modal',
-  imports: [],
+  imports: [ModalComponent, CommonModule, ReactiveFormsModule, DynamicFieldComponent],
   templateUrl: './edit-department-modal.component.html',
-  styleUrl: './edit-department-modal.component.css'
 })
-export class EditDepartmentModalComponent {
+export class EditDepartmentModalComponent implements OnInit {
+
+  @Input() department: Department | undefined;
+  @Output() closeModal = new EventEmitter<void>();
+  @Output() errorMessage = new EventEmitter<string>();
+  @Output() editDepartment = new EventEmitter<Department>()
+
+  form!: FormGroup;
+
+  inputGroups: Field[][] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private departmentService: DepartmentService
+  ) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      name: [this.department?.name || '', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]]
+    });
+
+    this.inputGroups = [
+      [
+        {
+          label: 'Name',
+          name: 'name',
+          type: 'input',
+          inputType: 'text',
+          placeholder: 'Mechanic',
+          value: this.department?.name || ''
+        }
+      ]
+    ];
+  }
+
+  onClose(): void {
+    this.closeModal.emit();
+  }
+
+  onSave():void {
+
+    if(this.form.invalid) {
+          this.errorMessage.emit('Fill in all required fields.')
+          return
+    }
+
+    const editDepartment: Department = this.form.value;
+
+  }
 
 }
