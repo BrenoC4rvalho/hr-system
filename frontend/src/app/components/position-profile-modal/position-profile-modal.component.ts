f2956139506fb16f3abcc9d8c59a19957eccb51c
BrenoC4rvalho <bc101004@gmail.com>
2025-05-17 17:48:30 -0300

    refactor: position profile.

--- frontend/src/app/components/position-profile-modal/position-profile-modal.component.ts
+++ frontend/src/app/components/position-profile-modal/position-profile-modal.component.ts
@@ -15,44 +15,9 @@ export class PositionProfileModalComponent {
 
   @Input() position: Position | undefined;
   @Output() closeModal = new EventEmitter<void>();
-  @Output() errorMessage = new EventEmitter<string>();
-
-  isEditing = false;
-  editablePosition: Position = {} as Position;
-
-  constructor(private positionService: PositionService) {}
 
   onClose(): void {
     this.closeModal.emit();
   }
 
-  toggleEdit(): void {
-    if (this.position) {
-      this.editablePosition = { ...this.position };
-      this.isEditing = true;
-    }
-  }
-
-  cancelEdit(): void {
-    this.isEditing = false;
-  }
-
-  saveChanges(): void {
-    if (!this.editablePosition.name || this.editablePosition.name.length < 3) {
-      this.errorMessage.emit('Position name must be at least 3 characters long.');
-      return;
-    }
-
-    if (this.position?.id) {
-      this.positionService.update(this.position.id, this.editablePosition).subscribe({
-        next: (updatedPosition: Position) => {
-          this.position = updatedPosition;
-          this.isEditing = false;
-        },
-        error: (err) => {
-          this.errorMessage.emit(err?.error || 'Failed to update position.');
-        }
-      });
-    }
-  }
 }
