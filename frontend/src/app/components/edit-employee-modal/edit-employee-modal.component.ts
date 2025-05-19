baf4f64b1cedb818a4ef3d2638856786106bdb6c
BrenoC4rvalho <bc101004@gmail.com>
2025-05-18 22:38:13 -0300

    feat: implent init template and logic for edit employee.

--- frontend/src/app/components/edit-employee-modal/edit-employee-modal.component.ts
+++ frontend/src/app/components/edit-employee-modal/edit-employee-modal.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ModalComponent } from "../../shared/modal/modal.component";
+import { Employee } from '../../core/model/employee';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Field } from '../../core/types/Field';
+import { EmployeeService } from '../../core/service/employee.service';
+import { DynamicFieldComponent } from "../dynamic-field/dynamic-field.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-employee-modal',
-  imports: [],
+  imports: [ModalComponent, DynamicFieldComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './edit-employee-modal.component.html',
-  styleUrl: './edit-employee-modal.component.css'
 })
 export class EditEmployeeModalComponent {
+
+
+  @Input() employee: Employee | undefined;
+  @Output() closeModal = new EventEmitter<void>();
+  @Output() errorMessage = new EventEmitter<string>();
+  @Output() editEmployee = new EventEmitter<Employee>()
+
+  form!: FormGroup;
+
+  inputGroups: Field[][] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private employeeService: EmployeeService
+  ) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+    })
+  }
+
+  onClose(): void {
+    this.closeModal.emit();
+  }
+
+  onSave():void {
+
+    if(this.form.invalid) {
+          this.errorMessage.emit('Fill in all required fields.')
+          return
+    }
+
+    const editEmployee: Employee = this.form.value;
+
+  }
 
 }
