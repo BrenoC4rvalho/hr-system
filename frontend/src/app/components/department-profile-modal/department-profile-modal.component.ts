89768ff1ab2fec4204064f184dabbf413b81a8c5
BrenoC4rvalho <bc101004@gmail.com>
2025-05-19 21:50:09 -0300

    feat: edit department profile.

--- frontend/src/app/components/department-profile-modal/department-profile-modal.component.ts
+++ frontend/src/app/components/department-profile-modal/department-profile-modal.component.ts
@@ -10,63 +10,14 @@ import { FormsModule } from '@angular/forms';
   imports: [ModalComponent, CommonModule, FormsModule],
   templateUrl: './department-profile-modal.component.html',
 })
-export class DepartmentProfileModalComponent implements OnChanges {
+export class DepartmentProfileModalComponent {
 
-  @Input() departmentId: number | undefined;
+  @Input() department: Department | undefined;
   @Output() closeModal = new EventEmitter<void>();
   @Output() errorMessage = new EventEmitter<string>();
-
-  department: Department | null = null;
-  editableDepartment: Department = {} as Department;
-  isEditing = false;
-
-  constructor(private departmentService: DepartmentService) {}
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['departmentId'] && this.departmentId) {
-      this.loadDepartment();
-    }
-  }
-
-  loadDepartment(): void {
-    if (!this.departmentId) return;
-    this.departmentService.show(this.departmentId).subscribe({
-      next: (department: Department) => {
-        this.department = department;
-        this.editableDepartment = { ...department };
-      },
-      error: () => {
-        this.errorMessage.emit('Failed to load department');
-      }
-    });
-  }
-
-  toggleEdit(): void {
-    this.isEditing = true;
-  }
-
-  cancelEdit(): void {
-    if (this.department) {
-      this.editableDepartment = { ...this.department };
-    }
-    this.isEditing = false;
-  }
-
-  saveChanges(): void {
-    if (!this.departmentId) return;
-
-    this.departmentService.update(this.departmentId, this.editableDepartment).subscribe({
-      next: (updatedDept) => {
-        this.department = updatedDept;
-        this.isEditing = false;
-      },
-      error: () => {
-        this.errorMessage.emit('Failed to save changes.');
-      }
-    });
-  }
 
   onClose(): void {
     this.closeModal.emit();
   }
+
 }
