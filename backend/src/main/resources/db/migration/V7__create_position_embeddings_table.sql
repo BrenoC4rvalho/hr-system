bbf78fa717f5b101ef26263876c7c844b93d059d
BrenoC4rvalho <bc101004@gmail.com>
2025-06-21 22:51:50 -0300

    feat(db): Create position_embeddings table.

---
+++ backend/src/main/resources/db/migration/V7__create_position_embeddings_table.sql
@@ -0,0 +1,14 @@
+-- V7: create position embeddings table
+
+CREATE TABLE position_embeddings (
+    id UUID PRIMARY KEY,
+    position_id BIGINT NOT NULL,
+    context_chunk TEXT,
+    embedding VECTOR(768) NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_position
+        FOREIGN KEY(positon_id)
+        REFERENCES positions(id)
+        ON DELETE CASCADE
+);
