2733ef25da41a2de3e13b54fb2e0ad9b4a242cca
BrenoC4rvalho <bc101004@gmail.com>
2025-06-26 19:44:01 -0300

    feat: implement logic for chat.

--- frontend/src/app/components/chat/chat.component.ts
+++ frontend/src/app/components/chat/chat.component.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CircleX, LucideAngularModule, SendHorizontal } from 'lucide-angular';
 import { MessageComponent } from "../message/message.component";
+import { ChatService } from '../../core/service/chat.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
-  imports: [CommonModule, LucideAngularModule, MessageComponent],
+  imports: [CommonModule, FormsModule, LucideAngularModule, MessageComponent],
   templateUrl: './chat.component.html',
 })
 export class ChatComponent {
@@ -14,6 +16,12 @@ export class ChatComponent {
   readonly SendIcon = SendHorizontal;
 
   isVisible: boolean = false;
+  userMessage: string = '';
+  fullResponse: string = '';
+  errorMessage: string = '';
+  isLoading: boolean = false;
+
+  constructor(private chatService: ChatService) {}
 
   openChat(): void {
     this.isVisible = true;
@@ -21,6 +29,32 @@ export class ChatComponent {
 
   closeChat(): void {
     this.isVisible = false;
+  }
+
+  sendMessage(): void {
+    if(!this.userMessage.trim()) {
+      return
+    }
+
+    this.isLoading = true;
+    this.fullResponse = '';
+    this.errorMessage = '';
+    const messageToSend = this.userMessage;
+    this.userMessage = '';
+
+    this.chatService.generateResponse(messageToSend).subscribe({
+      next: (chunk) => {
+        this.fullResponse += chunk;
+      },
+      error: (err) => {
+        console.error('Chat API Error:', err);
+        this.errorMessage = 'An error occurred. Please try again.';
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.isLoading = false;
+      }
+    })
   }
 
 }
